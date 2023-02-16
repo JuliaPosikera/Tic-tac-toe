efe63d0812d0366250be1b8b0a6fcfefe2ccc870
JuliaPosikera <posikera.julia@gmail.com>
2023-02-16 13:57:36 +0100

    second try

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move
-        ? `Go to move # + ${move}. (Col ${col} Row ${row} )` + move
+        ? `Go to move # + ${move}. (Col ${step.col} Row ${step.row} )` + move
         : "Go to game start";
       return (
         <li key={move}>
